1dc4d140bf962e4e2796371b2ae20fcc3036cf30
Gennady Kurbesov <ltrg67@gmail.com>
2024-05-16 14:23:05 +0400

    tests

---
+++ Sandbox.Tests/TestClassTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox.Tests;
+
+[TestClass]
+public class TestClassTests
+{
+    [TestMethod]
+    public void Test()
+    {
+        Assert.Equals(1, 1);
+    }
+
+    [TestMethod]
+    public void Test2()
+    {
+        Assert.Equals(1, 1);
+    }
+}
